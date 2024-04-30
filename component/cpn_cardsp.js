d24c2ac57e123481a6cbd53071d65807650e5b43
trinhuyenlht7 <trinhuyenlht7@gmail.com>
2024-04-30 17:00:50 +0700

    hello I am Uyen

--- component/cpn_cardsp.js
+++ component/cpn_cardsp.js
@@ -1,16 +1,36 @@
-Vue.component('card_sp',{
-    props: ['sp'],
-    template : `<div class=" col-6 col-md-3 card_sp">
-    <div class="card_body">
-            <img :src="sp.hinh" style="width : 100%" alt="">
+Vue.component('card_sp', {
+    props: ['obj'],
+    template: `<div class="card_body">
+        <img :src="obj.hinh" style="width : 100%" alt="">
         <div class="if_sp">
-            <h4>{{sp.ten}}</h4><br>
-            <p>thongtin.sp</p>
+            <h4>{{sp.tensp}}</h4>
+            <div class="gia_giam">
+                <h4>{{giadagiam.obj}}</h4><span>vnđ</span>
+            </div>                                  
+            <div class="gia">
+                <p>{{giachuagiam.obj}}</p><span>vnđ</span>
+            </div>
         </div>
         <div class="btn_buy">
             <button type="button" class="btn btn-primary">Mua</button>
             <button type="button" class="btn btn-primary">Thêm</button>
         </div>
-    </div>
-</div>`
+    </div>`
 })
+
+
+var vm = new Vue({
+    el: '#app',
+    data: {
+        sanpham_iphone: [
+            {
+                hinh: 'https://placehold.co/250x300',
+                ten: 'Iphone 15 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            }
+        ]
+    }
+
+}
+)
