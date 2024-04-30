9f45d650f20e9e9fd9a4f84e7641d86ab608d8d1
trinhuyenlht7 <trinhuyenlht7@gmail.com>
2024-04-30 18:40:07 +0700

    hihi

--- component/cpn_cardsp.js
+++ component/cpn_cardsp.js
@@ -3,12 +3,12 @@ Vue.component('card_sp', {
     template: `<div class="card_body">
         <img :src="obj.hinh" style="width : 100%" alt="">
         <div class="if_sp">
-            <h4>{{sp.tensp}}</h4>
+            <h4>{{obj.tensp}}</h4>
             <div class="gia_giam">
-                <h4>{{giadagiam.obj}}</h4><span>vnđ</span>
+                <h4>{{obj.giadagiam}}</h4><span>vnđ</span>
             </div>                                  
             <div class="gia">
-                <p>{{giachuagiam.obj}}</p><span>vnđ</span>
+                <p>{{obj.giachuagiam}}</p><span>vnđ</span>
             </div>
         </div>
         <div class="btn_buy">
@@ -22,14 +22,98 @@ Vue.component('card_sp', {
 var vm = new Vue({
     el: '#app',
     data: {
-        sanpham_iphone: [
+        iphone: [
             {
                 hinh: 'https://placehold.co/250x300',
-                ten: 'Iphone 15 promax',
+                tensp: 'Iphone 11 promax',
                 giadagiam : '19.000.000',
                 giachuagiam : '30.000.000'
-            }
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 12 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 13 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 14 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },{
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 15 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 16 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 17 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 18 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 19 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'Iphone 20 promax',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+        ],
+
+        mac : [
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'MACBOOK PRO',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'MACBOOK PRO',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'MACBOOK PRO',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
+            {
+                hinh: 'https://placehold.co/250x300',
+                tensp: 'MACBOOK PRO',
+                giadagiam : '19.000.000',
+                giachuagiam : '30.000.000'
+            },
         ]
+    },
+    method: {
+
     }
 
 }
